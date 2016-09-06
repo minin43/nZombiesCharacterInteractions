3280dc09281c6c3511923fc717f008621cd819bf
Logan <lobsterlogan43@yahoo.com>
2016-09-06 12:01:01 -0500

    nz

--- lua/autorun/client/cl_weaponhooks.lua
+++ lua/autorun/client/cl_weaponhooks.lua
@@ -2,3 +2,11 @@
 net.Receive( "", function()
 
 end )
+
+net.Receive( "PurchaseFailure", function()
+    local number = net.ReadInt()
+	whatsmyname()
+	timer.Simple( 0.9, function()
+		sound.Play( "nz/"..charactername.."/Money/no_money"..number..".ogg", self:GetPos(), 75, 100, 1 )
+	end )
+end )
--- lua/autorun/server/sv_weaponhooks.lua
+++ lua/autorun/server/sv_weaponhooks.lua
@@ -4,8 +4,24 @@ local function typeofweapon( wep )
     }
 end
 
-hook.Add( "OnPlayerBuy", "", function( ply, amount, wep, func ) 
+util.AddNetworkString( "PurchaseSuccess" )
+hook.Add( "OnPlayerBought", "PurchaseSuccess", function( ply, price, ent ) 
 
+end )
+
+util.AddNetworkString( "PurchaseFailure" )
+hook.Add( "OnPlayerBuy", "PurchaseFailure", function( ply, price, ent, func ) 
+    if ply:CanAfford( price ) then
+        return
+    else
+        if !timer.Exists( ply:SteamID().."timer" ) then
+		    timer.Create( ply:SteamID().."timer", 0.1, 1, function() --All dialogue delays are done client-side
+			    net.Start( "PurchaseFailure" )
+				    net.WriteInt( math.random( 1, 3 ), 3 ) --There are 3 quotes from each character
+			    net.Send( ply )
+		    end )
+	    end 
+    end
 end )
 
 hook.Add( "WeaponEquip", "AmmoCheck", function( wep ) 
